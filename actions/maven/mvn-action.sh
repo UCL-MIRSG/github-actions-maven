2dcb2c78213ed1d4ce4075ca8950357dabd2e295
Tom Doel <tomdoel@users.noreply.github.com>
2021-10-25 10:07:24 +0100

    Remove setting of JAVA_HOME as this does not appear to be correct

--- actions/maven/mvn-action.sh
+++ actions/maven/mvn-action.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo "JAVA_HOME = $JAVA_HOME"
-JAVA_HOME="/usr/local/openjdk-11/"
 # Setup maven local repo
 if [[ -n "$MAVEN_LOCAL_REPO_PATH" ]]; then
      MAVEN_REPO_LOCAL="-Dmaven.repo.local=$MAVEN_LOCAL_REPO_PATH"
